cc231ac4affdf4e75274b69f38fe1d221874e910
AdornJoshi <94985275+AdornJoshi@users.noreply.github.com>
2023-06-11 09:10:21 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const First = ()=> {
+  return (
+    <div>
+      <h1 color='skyblue'>Login</h1>
+      <h5>Enter your name</h5>
+      <input placeholder="name" input/>
+      <br></br>
+      <h5>Enter your place</h5>
+      <input placeholder="place" input/>
+      <br></br>
+      <h5 color='white'>Enter your age</h5>
+      <input placeholder="age" input/>
+      <br></br><br></br>
+      <h5 color='white'>Enter your emailid</h5>
+      <input placeholder="id" input/>
+      <br></br><br></br>
+      <h5 color='white'>Enter your contact details</h5>
+      <input placeholder="No" input/>
+      <br></br><br></br>
+      <h5 color='white'>Enter your phone number</h5>
+      <input placeholder="num" input/>
+      <br></br><br></br>
+      <button color='white'>Login</button>
+      <br></br><br></br>
+  
+   
+    </div>
+  )
+}
+
+export default First
